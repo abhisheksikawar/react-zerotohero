8d1843d9a19c6992ff023a181728fa1e26019e56
abhisheksikarwar <abhisheksikarwar15@gmail.com>
2024-04-04 10:56:23 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,54 @@
+// const heading= React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Hello World From React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//Example 2
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    [
+        React.createElement(
+            "div",
+            {id:"child"},
+            [React.createElement(
+                "h1",
+                {},
+                "I am a heading h1 tag"
+                
+            ),
+            React.createElement(
+                "h2",
+                {},
+                "I am a heading h2 tag"
+                
+            )
+        ]
+            
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [React.createElement(
+                "h1",
+                {},
+                "I am a heading h1 tag"
+                
+            ),
+            React.createElement(
+                "h2",
+                {},
+                "I am a heading h2 tag"
+                
+            )
+        ]
+            
+        )
+    ]
+    
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
